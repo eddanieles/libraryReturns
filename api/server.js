ea8f03e310118ab0ee4f91dc9d07cc4ab92d1d70
eddanieles <eddanieles@gmail.com>
2020-12-09 18:20:07 -0500

    added createBook component

--- api/server.js
+++ api/server.js
@@ -13,8 +13,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../webApp/dist')));
 
 app.get('/api/books', (req, res) => {
-    console.log(res);
-    console.log(books);
     res.json(books);
 });
 
--- webApp/src/views/Books.vue
+++ webApp/src/views/Books.vue
@@ -2,22 +2,29 @@
   <div class="books">
     <h1>Books</h1>
     <button @click='getAllBooks()' type="button" class="btn btn-warning">Get all Books</button>
+    <div>{{books}}</div>
   </div>
 </template>
 
 <script>
-  // import { getAllBooks } from '../services/BookService'
+  import { getAllBooks } from '../services/BookService'
   export default {
-    // data() {
-    //     return {
-    //         books: []
-    //     }
-    // },
+    data() {
+        return {
+            books: []
+        }
+    },
     methods: 
     {
       getAllBooks() {
-        this.$emit('getAllBooks')
-      },
+        getAllBooks().then(response => {
+          console.log(response)
+          this.books = response
+        })
+      }
+      // getAllBooks() {
+      //   this.$emit('getAllBooks')
+      // },
     },
     mounted () {
       this.getAllBooks();
