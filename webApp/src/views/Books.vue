073b17a234fadbb8bed01010b20db1715b03edac
eddanieles <eddanieles@gmail.com>
2020-12-09 16:23:32 -0500

    books view

--- webApp/src/views/Books.vue
+++ webApp/src/views/Books.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="books">
     <h1>Books</h1>
+    <button @click='getAllBooks()' type="button" class="btn btn-warning">Get all Books</button>
   </div>
 </template>
 
 <script>
-import { getAllBooks } from '../services/BookService'
-
-export default {
-  data() {
-      return {
-          books: []
-      }
-  },
-  methods: 
-  {
-    getAllBooks() {
-      getAllBooks().then(response => {
-        console.log(response);
-        this.books = response;
-      })
+  // import { getAllBooks } from '../services/BookService'
+  export default {
+    // data() {
+    //     return {
+    //         books: []
+    //     }
+    // },
+    methods: 
+    {
+      getAllBooks() {
+        this.$emit('getAllBooks')
+      },
+    },
+    mounted () {
+      this.getAllBooks();
     }
-  },
-  mounted () {
-    this.getAllBooks();
   }
-}
 </script>
