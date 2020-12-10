664398ca0f77b1a51a398367098550a162c06f12
eddanieles <eddanieles@gmail.com>
2020-12-10 04:13:50 -0500

    added underscore

--- webApp/src/components/Aggregrate.vue
+++ webApp/src/components/Aggregrate.vue
@@ -3,18 +3,21 @@
         <div>
             Total Pages Read: {{totalPagesRead}}
         </div>
-        <div>{{cats["500"]}}</div>
+        <div>**category example: {{pagesReadByCatergory}}</div>
     </div>
 </template>
 
 <script>
     import { getAllBooks, deweyCategories } from '../services/BookService'
+    import _ from 'underscore'
+
     export default {
         data() {
             return {
                 books: [],
                 cats: deweyCategories,
-                totalPagesRead: 0
+                totalPagesRead: 0,
+                pagesReadByCatergory: {},
             }
         },
         methods: 
@@ -23,14 +26,31 @@
                 getAllBooks().then(response => {
                     this.books = response;
                     this.totalPagesRead = response.map((book) => {
-                        if (book.amountRead === "unread") book.numberOfPages = 0;
-                        else if (book.amountRead === "partially") book.numberOfPages = parseInt(book.numberOfPages)/2;
-                        return parseInt(book.numberOfPages)
+                        if (book.amountRead === "unread") book.pagesRead = 0;
+                        else if (book.amountRead === "partially") book.pagesRead = parseInt(book.numberOfPages)/2;
+                        else if (book.amountRead === "read") book.pagesRead = parseInt(book.numberOfPages);
+
+                        if (!book.dewey) book.category = "Category Not Provided"
+                        else if (/^0/.test(book.dewey)) book.category = this.cats["000"];
+                        else if (/^1/.test(book.dewey)) book.category = this.cats["100"];
+                        else if (/^2/.test(book.dewey)) book.category = this.cats["200"];
+                        else if (/^3/.test(book.dewey)) book.category = this.cats["300"];
+                        else if (/^4/.test(book.dewey)) book.category = this.cats["400"];
+                        else if (/^5/.test(book.dewey)) book.category = this.cats["500"];
+                        else if (/^6/.test(book.dewey)) book.category = this.cats["600"];
+                        else if (/^7/.test(book.dewey)) book.category = this.cats["700"];
+                        else if (/^8/.test(book.dewey)) book.category = this.cats["800"];
+                        else if (/^9/.test(book.dewey)) book.category = this.cats["900"];
+
+                        return parseInt(book.pagesRead)
                     })
                     .reduce((accumulator, currentValue) => { 
                         return parseInt(accumulator) + parseInt(currentValue)
                     });
-                })
+
+                    this.pagesReadByCatergory = Object.entries(_.groupBy(response, 'category'));
+                    });
+                
             }
         },
         mounted () {
