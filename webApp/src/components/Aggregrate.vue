59482d16be1f8ed01f6bd44c84bfe755e8ea7fc5
eddanieles <eddanieles@gmail.com>
2020-12-10 02:35:12 -0500

    updated mapping function

--- webApp/src/components/Aggregrate.vue
+++ webApp/src/components/Aggregrate.vue
@@ -1,15 +1,7 @@
 <template>
     <div>
         <div>
-            Total Pages Read: {{books.map((book) => {
-                        if (book.amountRead === "unread") book.numberOfPages = 0;
-                        else if (book.amountRead === "partially") book.numberOfPages = parseInt(book.numberOfPages)/2;
-                        return book.numberOfPages
-                    })
-                    .reduce((accumulator, currentValue, currentIndex, array) => { 
-                        return parseInt(accumulator) + parseInt(currentValue)
-                    })
-            }}
+            Total Pages Read: {{totalPagesRead}}
         </div>
         <div>{{cats["500"]}}</div>
     </div>
@@ -21,7 +13,8 @@
         data() {
             return {
                 books: [],
-                cats: deweyCategories
+                cats: deweyCategories,
+                totalPagesRead: 0
             }
         },
         methods: 
@@ -29,11 +22,20 @@
             initializeBooks() {
                 getAllBooks().then(response => {
                     this.books = response;
+                    this.totalPagesRead = response.map((book) => {
+                        if (book.amountRead === "unread") book.numberOfPages = 0;
+                        else if (book.amountRead === "partially") book.numberOfPages = parseInt(book.numberOfPages)/2;
+                        return parseInt(book.numberOfPages)
+                    })
+                    .reduce((accumulator, currentValue) => { 
+                        return parseInt(accumulator) + parseInt(currentValue)
+                    });
                 })
             }
         },
         mounted () {
             this.initializeBooks();
+
         }
     }
 </script>
